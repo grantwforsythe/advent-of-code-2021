a5cb596e818b02dfbab84452a05cdb9b935f7962
Grant Forsythe <grantwforsythe@gmail.com>
2022-01-13 23:57:13 -0800

    Day 02

---
+++ day02/aoc2p1.py
@@ -0,0 +1,30 @@
+from load_data import load_data
+
+
+def main():
+    # starting positions
+    horizontal = 0
+    depth = 0
+
+    for line in load_data():
+        line = line.strip()
+        val = int(line[-1])
+
+        match line[0]:
+            # forward
+            case 'f':
+                horizontal += val
+            # down
+            case 'd':
+                depth += val
+            # up
+            case 'u':
+                depth -= val
+            case _:
+                raise ValueError("There is more than 3 positions!")
+
+    print(f'Answer: {horizontal * depth}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ day02/aoc2p2.py
@@ -0,0 +1,32 @@
+from load_data import load_data
+
+
+def main():
+    # starting positions
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for line in load_data():
+        line = line.strip()
+        val = int(line[-1])
+
+        match line[0]:
+            # forward
+            case 'f':
+                horizontal += val
+                depth += aim * val
+            # down
+            case 'd':
+                aim += val
+            # up
+            case 'u':
+                aim -= val
+            case _:
+                raise ValueError("There is more than 3 positions!")
+
+    print(f'Answer: {horizontal * depth}')
+
+
+if __name__ == '__main__':
+    main()
